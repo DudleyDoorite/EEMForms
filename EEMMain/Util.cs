6ea6d67332b58c498d8b56a3fa7b41566252f1f9
Glen Dudley <glen.dudley@gmail.com>
2018-02-26 21:39:15 -0500

    Fix For Issue #6 - Clone function should "guess" new folder name Added Unit Testing - Implimented Added Data Driven Testing - Not Implimented

---
+++ EEMMain/Util.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EEMMain
+{
+    public class Util
+    {
+        public static string GetNextFolder(string folder)
+        {
+            string ret = string.Empty;
+            string lastTwo;
+            string firstHalf;
+            int LastTwoValue;
+
+
+            //Get the last 2 char of the folder name passed in.
+            if (folder.Length > 2)
+            {
+                lastTwo = folder.Substring(folder.Length - 2);
+                firstHalf = folder.Substring(0, folder.Length - 2);
+                try
+                {
+                    LastTwoValue = int.Parse(lastTwo)+1;
+                    ret = string.Format("{0}{1}", firstHalf, LastTwoValue.ToString("00"));
+                }
+                catch (Exception ex)
+                {
+                    LastTwoValue = 0;
+                    ret = string.Format("{0}{1}", folder, LastTwoValue.ToString("00"));
+                }
+            }
+            else
+            {
+                ret = string.Format("{0}00", folder);
+            }
+
+
+            return ret;
+        }
+    }
+}
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EEMMain;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class UnitTest_Util
+    {
+        public UnitTest_Util()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+
+        //[TestMethod]
+        //[DeploymentItem("|ProjectName|\\TestData\\Util_GetNextFolder.xml")]
+        //[DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+        //           "|DataDirectory|\\Util_GetNextFolder.xml",
+        //           "FolderTest",
+        //            DataAccessMethod.Sequential)]
+        //public void TestMethod_GetNextFolderDDT()
+        //{
+        //    string target = (string)this.TestContext.DataRow["Expected"];
+        //    string inputFolder = (string)this.TestContext.DataRow["Input"]; ;
+        //    string actual = string.Empty;
+        //    actual = Util.GetNextFolder(inputFolder);
+        //    Console.WriteLine("Input:{0}  Target:{1}  Actual:{2}", inputFolder, target, actual);
+        //    Assert.IsTrue(target == actual);
+        //}
+
+
+        [TestMethod]
+        public void TestMethod_GetNextFolder()
+        {
+            string target = "F00";
+            string inputFolder = "F";
+            string actual = string.Empty;
+            actual = Util.GetNextFolder(inputFolder);
+            Console.WriteLine("Input:{0}  Target:{1}  Actual:{2}", inputFolder, target, actual);
+            Assert.IsTrue(target == actual);
+        }
+
+        [TestMethod]
+        public void TestMethod_GetNextFolder01()
+        {
+            string target = "Sample00";
+            string inputFolder = "Sample";
+            string actual = string.Empty;
+            actual = Util.GetNextFolder(inputFolder);
+            Console.WriteLine("Input:{0}  Target:{1}  Actual:{2}", inputFolder, target, actual);
+            Assert.IsTrue(target == actual);
+        }
+
+        [TestMethod]
+        public void TestMethod_GetNextFolder02()
+        {
+            string target = "Sample04";
+            string inputFolder = "Sample03";
+            string actual = string.Empty;
+            actual = Util.GetNextFolder(inputFolder);
+            Console.WriteLine("Input:{0}  Target:{1}  Actual:{2}", inputFolder, target, actual);
+            Assert.IsTrue(target == actual);
+        }
+
+        [TestMethod]
+        public void TestMethod_GetNextFolder03()
+        {
+            string target = "S04";
+            string inputFolder = "S03";
+            string actual = string.Empty;
+            actual = Util.GetNextFolder(inputFolder);
+            Console.WriteLine("Input:{0}  Target:{1}  Actual:{2}", inputFolder, target, actual);
+            Assert.IsTrue(target == actual);
+        }
+
+        [TestMethod]
+        public void TestMethod_GetNextFolder04()
+        {
+            string target = "xyz00";
+            string inputFolder = "xyz";
+            string actual = string.Empty;
+            actual = Util.GetNextFolder(inputFolder);
+            Console.WriteLine("Input:{0}  Target:{1}  Actual:{2}", inputFolder, target, actual);
+            Assert.IsTrue(target == actual);
+        }
+
+        [TestMethod]
+        public void TestMethod_GetNextFolder05()
+        {
+            string target = "xyz01";
+            string inputFolder = "xyz00";
+            string actual = string.Empty;
+            actual = Util.GetNextFolder(inputFolder);
+            Console.WriteLine("Input:{0}  Target:{1}  Actual:{2}", inputFolder, target, actual);
+            Assert.IsTrue(target == actual);
+        }
+
+
+    }
+}
